f337b8eb34017271112129c1609d6d5cc274622d
its julii <nanji828@gmail.com>
2022-11-14 21:57:05 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Number_in_Range
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num = double.Parse(Console.ReadLine());
+
+            if (num != 0 && num >= -100 && num <= 100)
+            {
+                Console.WriteLine("Yes");
+            }
+            else 
+                    {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
